ee351a0bb0dec0c8f7c3bbec06a47d7989889781
sourav <sanjusourav455@gmail.com>
2026-01-21 20:21:36 +0530

    Fix connectDB default export

--- app/api/products/add/route.js
+++ app/api/products/add/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import Product from "@/models/Product";
-import { connectDB } from "@/lib/db";
+import connectDB from "@/lib/db";
+
 
 export async function POST(req) {
   try {
